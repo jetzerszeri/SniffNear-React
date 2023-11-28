8e78452053e73d0e113072fdf29f8aa7a8c83e6f
jetzerszeri <jetzers.2@gmail.com>
2023-11-28 18:03:55 -0500

    feat: get user info from db to home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 import { Navbar } from '../components/Navbar'
 import { WelcomeCard } from '../components/home/WelcomeCard'
 import { MyPetsCards } from '../components/home/MyPetsCards'
 import { ServicesCards } from '../components/home/ServicesCards'
 import { BottomNav } from '../components/BottomNav'
 
+const storedUserId = localStorage.getItem('userId');
+console.log(storedUserId);
+
+const getUserInfo = async () => {
+  const response = await fetch(`https://sniffnear-api.onrender.com/api/users/${storedUserId}`);
+  if(response.ok){
+    const  data  = await response.json();
+    console.log(data);
+  } 
+}
+
+
+
+
 export const Home = () => {
+
+  useEffect(() => {
+    getUserInfo();
+  }, [])
+  
+  
   return (
     <>
       <Navbar />
