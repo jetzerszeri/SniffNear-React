e7798c49233687b41f5442ce39116ee9f0c75f56
ailenad <ailen.alvarez@davinci.edu.ar>
2024-02-12 17:13:22 -0300

    mapa

--- src/components/alerts/map/MapLost.js
+++ src/components/alerts/map/MapLost.js
@@ -1,35 +1,46 @@
-import { useState } from "react";
-import { 
-APIProvider,
-Map,
-AdvancedMarker, 
-Pin,
-InfoWindow
-} from "@vis.gl/react-google-maps";
-export default function Intro (){
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   const { latitude, longitude } = position.coords;
-    //   this.setState({ currentLocation: { lat: latitude, lng: longitude } });
-    // });
-    const position = {lat:-32.855091 , lng:-68.893144};
-    const [open,setOpen] = useState(false);
+import React, { useState, useEffect } from "react";
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow} from "@vis.gl/react-google-maps";
 
-    return(
-        <APIProvider apiKey={"AIzaSyDYLirWViZkclvbf15XO8IJHY1KSo679tQ"}>
-            <div className="mapacontainer">
-            <div id="mi_mapa" >
-                <Map zoom={9} center={position}>
-                    <AdvancedMarker position={position} onClick={()=>{setOpen(true)}}>
-                        <Pin background={'red'} />
-                    </AdvancedMarker>
-                </Map>
-                {open && (
-                <InfoWindow position={position} onCloseClick={()=>{setOpen(false)}}>
-                    <p>Lo que le voy a poner a la alerta</p>
-                </InfoWindow>
-                )}
-            </div>
-            </div>
-        </APIProvider>
-    )
+export default function Intro() {
+  const [open, setOpen] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.error("Error obteniendo la ubicación: ", error.message);
+          // Aquí podrías mostrar un mensaje al usuario o realizar otra acción
+        }
+      );
+    } else {
+      console.log("Geolocalización no está disponible en este navegador.");
+    }
+  }, []);
+
+  return (
+    <APIProvider apiKey={"AIzaSyDYLirWViZkclvbf15XO8IJHY1KSo679tQ"}>
+      <div className="mapacontainer">
+        <div id="mi_mapa">
+          <Map zoom={9} center={currentLocation}>
+            {currentLocation && (
+              <AdvancedMarker position={currentLocation} onClick={() => setOpen(true)}>
+                <Pin background={"red"} />
+              </AdvancedMarker>
+            )}
+          </Map>
+          {open && (
+            <InfoWindow position={currentLocation} onCloseClick={() => setOpen(false)}>
+              <p>Lo que le voy a poner a la alerta</p>
+            </InfoWindow>
+          )}
+        </div>
+      </div>
+    </APIProvider>
+  );
 }
+
