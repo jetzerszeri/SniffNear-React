05abb4dcfca03f86de3f2686865973386e153d6b
ailenad <ailen.alvarez@davinci.edu.ar>
2024-02-20 16:34:55 -0300

    Filtrado de alertas por ubicacion

--- src/views/Alerts.js
+++ src/views/Alerts.js
@@ -4,27 +4,92 @@ import { AlertList } from "../components/alerts/AlertList"
 import { BottomNav } from "../components/BottomNav";
 import {getCurrentUserId, createLoader, removeLoader} from '../js/functions';
 import { Link } from "react-router-dom";
+import { 
+    setDefaults,
+    geocode,
+    RequestType,
+  } from "react-geocode";
 export const Alerts = () => {
+   const [alerts, setAlerts] = useState([]);
+//geolocalizacion
+const [lat,setLat] = useState(null);
+const [lng, setLng] = useState(null);
+const [status, setStatus] = useState(null);
+const [city, setCity] = useState('');
+const [state, setState] = useState('');
+const [country, setCountry] = useState('');
+const apiKey = process.env.REACT_APP_API_KEY_GEOCODING;
+//obtengo coordenadas
+useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          setStatus(null);
+        },
+        () => {
+          setStatus('No se pudo obtener tu ubicación');
+        }
+      );
+    } else {
+      setStatus('La geolocalización no es compatible con tu navegador');
+    }
+  }, []);
 
-    const [alerts, setAlerts] = useState([]);
+  setDefaults({
+    key: apiKey,
+    language:"es",
+    region: "es"
+  })
+  useEffect(()=>{
+    //obtengo la direccion desde las lat y lng
+            geocode(RequestType.LATLNG,`${lat}, ${lng}`,{
+            location_type: "ROOFTOP", // Override location type filter for this request.
+            enable_address_descriptor: true, // Include address descriptor in response.
+            })
+            .then(({ results }) => {
+            const address = results[0].formatted_address;
+            const { city, state, country } = results[0].address_components.reduce(
+                (acc, component) => {
+                if (component.types.includes("locality"))
+                    acc.city = component.long_name;
+                else if (component.types.includes("administrative_area_level_1"))
+                    acc.state = component.long_name;
+                else if (component.types.includes("country"))
+                    acc.country = component.long_name;
+                return acc;
+                },
+                {}
+            );
+            setCity(city);
+            setCountry(country);
+            setState(state);
+            })
+            .catch(console.error);
+})
+useEffect(() => {
+    const getAlerts = async () => {
+        try {
+            createLoader('Cargando alertas');
+            const response = await fetch(`https://sniffnear-api.onrender.com/api/alerts/`);
+            if (response.ok) {
+                const data = await response.json();
+                const filteredAlerts = data.filter((alert) => alert.city === city && alert.country === country);
+                setAlerts(filteredAlerts);
+                console.log(filteredAlerts)
+                removeLoader();
+            }
+        } catch (error) {
+            console.error('Error fetching data', error)
+        }
+    }
 
-    console.log(getCurrentUserId());
-    const getAlerts = useCallback( async () => {
-        createLoader('Cargando alertas');
-        const response = await fetch(`https://sniffnear-api.onrender.com/api/alerts/`);
-        if(response.ok){
-          const  data  = await response.json();
-          setAlerts(data);
-          removeLoader();
-         console.log(data) 
-        } 
-        
-    },[])
-    
-    useEffect(() => {
-        getAlerts()
-    },[]);
-    
+    getAlerts();
+}, [city, country]);
+
+
+
     const handleAlertDelete = (id) => {
         const updatedAlerts = alerts.filter(alert => alert._id !== id);
         setAlerts(updatedAlerts);
